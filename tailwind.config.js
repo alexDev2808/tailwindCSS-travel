e37d6ec043aa25cd337d13cd6d54e21acd88fd76
AlexDev2808 <colores.2808.8000@gmail.com>
2023-04-03 12:35:34 -0600

    Creando la seccion de recomendados, agregando el plugin scrollbar-hide

--- tailwind.config.js
+++ tailwind.config.js
@@ -53,7 +53,8 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
-    require('prettier-plugin-tailwindcss')
+    require('prettier-plugin-tailwindcss'),
+    require('tailwind-scrollbar-hide'),
   ],
 }
 
