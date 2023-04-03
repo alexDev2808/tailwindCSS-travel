bef0b425e19d7a5c56304a24b8d1fdbed427129d
AlexDev2808 <colores.2808.8000@gmail.com>
2023-04-03 08:53:38 -0600

    Trabajando con el width y height

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
         "hunt-yellow-300": "#FEFF86",
         "hunt-blue-300": "#B0DAFF",
         "hunt-blue-200": "#B9E9FC",
-        "hunt-blue-100": "#B9E9FC"
+        "hunt-blue-100": "#B9E9FC",
+        "hunt-lila-400": "#C04A82",
+        "hunt-lila-200": "#BE6DB7",
+        "hunt-rose-100": "#FFAACF",
+        "hunt-salmon-200": "#FAAB78"
       }
     },
   },
